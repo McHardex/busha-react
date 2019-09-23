906b97758092d78c12e8c14de29f21ee8b54be45
Oluwabukunmi Adebisi <oluwabukunmiadebisi@Oluwabukunmis-MacBook.local>
2019-09-23 06:41:34 +0100

    - create table - create body component hoc

--- src/views/Page2.jsx
+++ src/views/Page2.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import Layout from '../hoc/Layout';
+import React from "react";
+import Layout from "../hoc/Layout/Layout";
+import Account from "../hoc/Account/Account";
+import Sidebar from "../components/shared/SideBar/Sidebar";
+import Header from "../components/shared/Header/Header";
+import Transfers from "../components/Transfers/Transfers";
 
 const Page2 = () => {
   return (
     <Layout>
-      <p>this is the first page</p>
+      <Sidebar />
+      <div className="body">
+        <Header title="Tranfers" />
+        <Account>
+          <Transfers />
+        </Account>
+      </div>
     </Layout>
-  )
-}
+  );
+};
 
 export default Page2;
